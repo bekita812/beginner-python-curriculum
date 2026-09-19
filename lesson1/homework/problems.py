ec2fbd4af62cf9cd9dceaf8284b655300aec91f1
bekita812 <tehgasgebeslasi@gmail.com>
2026-09-19 11:02:27 -0700

    Fix variable names and formatting in problems.py

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,28 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
+lucky number = 16
 
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+number of hours of sleep = 10
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+my favorite fruit = mango 
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
 
-
+city = seattle 
+country= usa
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet name = sunny 
+pet age = 1 year 
+pet type = budgie
