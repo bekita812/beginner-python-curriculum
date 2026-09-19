d176ad7d7cd7611cceac0b43dc007b6468683da0
bekita812 <tehgasgebeslasi@gmail.com>
2026-09-19 11:14:42 -0700

    Fix variable declarations and formatting in practice.py

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,27 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
+favorite number = 24
 
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
+height= 1.3716 meters
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+favorite animal = peacock
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
-
+first name = Bekhiyta
+last name = Ahmedin
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+first name = bekhiyta
+my age = 10 
+favorite color = mint green
