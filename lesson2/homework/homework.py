76fa8ee14a964fe6ed6ab7e72a1a89020b5c9b26
bekita812 <tehgasgebeslasi@gmail.com>
2026-09-25 20:28:32 -0700

    Update homework.py

--- lesson2/homework/homework.py
+++ lesson2/homework/homework.py
@@ -2,25 +2,27 @@
 # Ask the user for two numbers.
 # Print their quotient and remainder on separate lines.
 
-
+i to the range (1)
+    print ''name two numbers''
 
 # Homework Problem 2
 # Ask the user for their favorite animal and favorite color.
 # Print a sentence combining them like: "A blue tiger would be awesome!"
-
+'' blue tuger are awesome '''
 
 
 # Homework Problem 3
 # Use a for loop to print all the even numbers from 0 to 10 (including 10).
 
 
-
+1 2 3 4 5 6 7 8 9 10
 # Homework Problem 4
 # Ask the user how many push-ups they can do.
 # Multiply it by 7 and print how many they could do in a week.
 
-
+49
 
 # Homework Problem 5
 # Use a for loop to print the square of each number from 1 to 6.
 # (Example: 1*1=1, 2*2=4, etc.)
+1 2 6 8 10 12 1
